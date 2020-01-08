bd3fed7c39601e2be44b8010c30a43e08f0ed5ce
Riju Nayak <rijun@thoughtworks.com>
2020-01-08 15:05:17 +0530

    Add. Write characterization test

--- src/test/java/com/thoughtworks/movierental/CustomerTest.java
+++ src/test/java/com/thoughtworks/movierental/CustomerTest.java
@@ -1,15 +1,31 @@
 package com.thoughtworks.movierental;
 
-import org.junit.Assert;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
 
 public class CustomerTest {
+
     @Test
-    public void test() {
+    public void should_correctly_show_the_statement() {
+        Customer customer = new Customer("Bruce Wayne");
+        Movie movieOne = new Movie("The Dark Knight", Movie.REGULAR);
+        Movie movieTwo = new Movie("Batman Begins", Movie.NEW_RELEASE);
+        Rental rentalOne = new Rental(movieOne, 5);
+        Rental rentalTwo = new Rental(movieTwo, 10);
+        customer.addRental(rentalOne);
+        customer.addRental(rentalTwo);
 
-        int expected = 3;
+        String statement = customer.statement();
 
-        Assert.assertEquals(expected, 1 + 2);
+        String expectedStatement = "Rental Record for Bruce Wayne\n" +
+                "\tThe Dark Knight\t6.5\n" +
+                "\tBatman Begins\t30.0\n" +
+                "Amount owed is 36.5\n" +
+                "You earned 3 frequent renter points";
+        System.out.println("expectedStatement = " + expectedStatement);
+        assertThat(statement, is(equalTo(expectedStatement)));
     }
-
 }
